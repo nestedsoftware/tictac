c056ae2114d50eab539a837f378f0b779b470bdd
Nested Software <2969361+nestedsoftware@users.noreply.github.com>
2019-07-01 21:37:19 -0600

    added transform.py - will be used to fix bug in qtable code. qtable code does not handle equivalent board positions properly since the qvalues are not transformed. this causes the qtable player to play invalid moves

---
+++ tests/test_transform.py
@@ -0,0 +1,18 @@
+import numpy as np
+from tictac.transform import Transform, Rotate, Flip
+
+
+def test_transform():
+    b = np.array([[1,  1, -1],
+                  [-1, 1,  1],
+                  [1, -1, -1]])
+    t = Transform(Rotate(2), Flip(np.flipud))
+
+    transformed_b = t.transform(b)
+
+    assert np.array_equal(transformed_b, np.array([[-1,  1,  1],
+                                               [1,   1, -1],
+                                               [-1, -1,  1]]))
+    reversed_b = t.reverse(transformed_b)
+
+    assert np.array_equal(reversed_b,  b)
---
+++ tictac/transform.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+class Transform:
+    def __init__(self, *operations):
+        self.operations = operations
+
+    def transform(self, target):
+        for op in self.operations:
+            target = op.transform(target)
+        return target
+
+    def reverse(self, target):
+        for op in reverse(self.operations):
+            target = op.reverse(target)
+        return target
+
+
+class Rotate:
+    def __init__(self, number_of_rotations):
+        self.number_of_rotations = number_of_rotations
+        self.op = np.rot90
+
+    def transform(self, matrix2d):
+        return self.op(matrix2d, self.number_of_rotations)
+
+    def reverse(self, transformed_matrix2d):
+        return self.op(transformed_matrix2d, -self.number_of_rotations)
+
+
+class Flip:
+    def __init__(self, op):
+        self.op = op
+
+    def transform(self, matrix2d):
+        return self.op(matrix2d)
+
+    def reverse(self, transformed_matrix2d):
+        return self.transform(transformed_matrix2d)
+
+
+def reverse(items):
+    return items[::-1]
