15a035dba20b3ad9f577376f30a2f37adfdd358f
Nested Software <2969361+nestedsoftware@users.noreply.github.com>
2019-08-03 18:50:15 -0600

    added to unit test

--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from tictac.board import BoardCache, Board
 from tictac.mcts import (perform_game_playout, find_or_create_node,
-                         calculate_values)
+                         calculate_values, perform_training_playouts)
 
 
 def test_play_mcts_move():
@@ -39,5 +39,32 @@ def test_play_mcts_move():
     expected_values = [(2, 1.0), (5, math.inf)]
     assert list(values) == expected_values
 
+    perform_game_playout(nc, board)
 
+    actual_stats = (parent_node.visits, parent_node.wins, parent_node.draws, parent_node.losses)
+    assert actual_stats == (2, 1, 0, 1)
 
+    actual_stats = (child_node_2.visits, child_node_2.wins, child_node_2.draws, child_node_2.losses)
+    assert actual_stats == (1, 1, 0, 0)
+
+    actual_stats = (child_node_5.visits, child_node_5.wins, child_node_5.draws, child_node_5.losses)
+    assert actual_stats == (1, 0, 0, 1)
+
+    values = calculate_values(nc, board)
+    expected_values = [(2, 2.177410022515475), (5, 1.1774100225154747)]
+    assert list(values) == expected_values
+
+    perform_training_playouts(nc, board, 100, False)
+
+    actual_stats = (parent_node.visits, parent_node.wins, parent_node.draws, parent_node.losses)
+    assert actual_stats == (102, 6, 0, 96)
+
+    actual_stats = (child_node_2.visits, child_node_2.wins, child_node_2.draws, child_node_2.losses)
+    assert actual_stats == (96, 96, 0, 0)
+
+    actual_stats = (child_node_5.visits, child_node_5.wins, child_node_5.draws, child_node_5.losses)
+    assert actual_stats == (6, 0, 0, 6)
+
+    values = calculate_values(nc, board)
+    expected_values = [(2, 1.3104087632087014), (5, 1.2416350528348057)]
+    assert list(values) == expected_values
